15dcfcd403991953c0ea59bb3b1bba6083207bb2
Mahbubur Rahman <mrrohanc@gmail.com>
2021-11-29 22:24:06 +0600

    delete yarn lock file

--- pages/component/Header.js
+++ pages/component/Header.js
@@ -1,15 +1,96 @@
-/* This example requires Tailwind CSS v2.0+ */
-import Image from 'next/image';
-import me from '../../public/me.jpg'
+import React from 'react';
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import me from '../../public/me.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faFacebookF, faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 
-export default function Header() {
+const Header = () => {
+    const mobileMenu = React.useRef();
+
+    const openMobileMenu = () => {
+        mobileMenu.current.style.left = "0";
+        mobileMenu.current.style.opacity = "100%";
+    }
+
+    const closeMobileMenu = () => {
+        mobileMenu.current.style.left = "-100%";
+        mobileMenu.current.style.opacity = "0%";
+    }
     return (
-        <nav>
+        <header>
+            <nav className="shadow-lg p-2 sm:py-3 sm:px-5">
+                {/* navbar */}
+                <div className="flex justify-between items-center">
+                    <div className="flex items-center">
+                        <Image src={me} width={40} height={40} alt="icon" className="rounded-full flex items-center justify-center logo" />
+                        <h4 className="ml-2 font-medium text-xl sm:text-2xl text-gray-700 ">MAHBUBUR RAHMAN</h4>
+                    </div>
+                    <div className="sm:hidden" onClick={openMobileMenu} >
+                        <FontAwesomeIcon icon={faBars} className="text-gray-700" height="50" />
+                    </div>
+                    {/* desktop menu */}
+                    <div className="hidden sm:block">
+                        <ul className="flex items-center gap-5 text-gray-500 uppercase font-semibold">
+                            <li><Link href="#"> Home</Link></li>
+                            <li><Link href="#"> Feature</Link></li>
+                            <li><Link href="#"> Portfolio</Link></li>
+                            <li><Link href="#"> Projects</Link></li>
+                            <li><Link href="#"> Blog</Link></li>
+                            <li><Link href="#"> Contact</Link></li>
+                            <li>
+                                <Link href="https://drive.google.com/uc?id=17xSMG2ZdrYPjV3PIgXvl7QsdC2AcirNI&export=download" >
+                                    <a className="py-4 px-5 rounded-lg outline-none default-btn default-gradient transition duration-500 linear text-blue-500 uppercase font-semibold">Download Resume</a>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            {/* mobile menu */}
+            <div className="mobile-popup-menu" ref={mobileMenu}>
+                <div className="mobile-menu-inner">
+                    <div className="flex justify-between items-center border-b-2 border-gray-400 p-2">
+                        <div className="flex items-center">
+                            <Image src={me} width={40} height={40} alt="icon" className="rounded-full flex items-center justify-center logo" />
+                            <h4 className="ml-2 font-medium text-base  sm:text-2xl text-gray-700 ">MAHBUBUR RAHMAN</h4>
+                        </div>
+                        <div className="rounded-full shadow-lg default-gradient h-8 w-8  flex justify-center items-center" onClick={closeMobileMenu}>
+                            <FontAwesomeIcon icon={faTimes} />
+                        </div>
+                    </div>
+                    <div className="p-2 mt-5">
+                        <ul className="text-gray-500 uppercase font-semibold flex flex-col gap-6">
+                            <li><Link href="#"> Home</Link></li>
+                            <li><Link href="#"> Feature</Link></li>
+                            <li><Link href="#"> Portfolio</Link></li>
+                            <li><Link href="#"> Projects</Link></li>
+                            <li><Link href="#"> Blog</Link></li>
+                            <li><Link href="#"> Contact</Link></li>
+                            <li><button className="py-4 px-5 rounded-lg outline-none default-btn default-gradient transition duration-500 linear text-blue-500 uppercase font-semibold"> Download Resume</button></li>
+                        </ul>
+                    </div>
+                    <div className="px-2 pb-5 absolute bottom-0">
+                        <h5 className="uppercase text-gray-700 font-semibold">Find With Me</h5>
+                        <div className="mt-3 flex gap-5">
+                            <div className="default-gradient flex justify-center items-center h-12 w-12 rounded">
+                                <div><FontAwesomeIcon icon={faFacebookF} className="text-blue-500" /></div>
+                            </div>
+                            <div className="default-gradient flex justify-center items-center h-12 w-12 rounded">
+                                <div><FontAwesomeIcon icon={faLinkedinIn} className="text-blue-500" /></div>
+                            </div>
+                            <div className="default-gradient flex justify-center items-center h-12 w-12 rounded">
+                                <div><FontAwesomeIcon icon={faGithub} className="text-blue-500" /></div>
+                            </div>
+                        </div>
 
-            this is navbar
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+};
 
-        </nav>
-
-
-    )
-}
+export default Header;
